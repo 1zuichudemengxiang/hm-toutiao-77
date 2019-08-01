d6684d9e748801bd02085fb6f5e986a0f4766528
1001 <1198809400@qq.com>
2019-08-01 21:29:42 +0800

    login加入卡片和logo

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,9 +1,46 @@
 <template>
-    <div class="container">login</div>
+    <div class="container">
+        <el-card class="my-card">
+            <img src="../../assets/images/logo_index.png" alt="">
+            <el-input class="phone" v-model="input" placeholder="请输入手机号"></el-input>
+        </el-card>
+    </div>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+    .container {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        background: url(../../assets/images/login_bg.jpg) no-repeat center / cover;
+    }
+
+    ;
+
+    .my-card {
+        width: 500px;
+        height: 500px;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+
+        img {
+            display: block;
+            width: 200px;
+            height: 50px;
+            margin: 0 auto;
+        }
+
+        .phone {
+            display: block;
+            margin: 60px auto;
+            width: 420px;
+        }
+
+    }
+</style>

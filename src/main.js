a649afd2088aab9110c846afc03567e8c498c195
1001 <1198809400@qq.com>
2019-08-01 20:33:53 +0800

    登录模块路由及组件

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// @是一个路径别名，src路径别名 在vue-cli创建的项目下才可是呀
+import router from '@/router'
 
+Vue.use(ElementUI)
 // 控制台提示信息，开发环境来提示，详细一些
 Vue.config.productionTip = false
 
@@ -9,5 +14,6 @@ Vue.config.productionTip = false
 // 2.创建一个根实例
 // 新建一个根实例
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
